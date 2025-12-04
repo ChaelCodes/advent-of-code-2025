f332c672f1b9732c1cbea3e33df2c7363850894d
Rachael Wright-Munn <hello@chael.codes>
2025-12-03 21:24:47 -0500

    Update parsing logic in sample solution.

--- solutions/day_00.rb
+++ solutions/day_00.rb
@@ -20,8 +20,8 @@ class Day00
   end
 
   def parse_records
-    records.each do |record|
-      self.records = PARSE_REGEX.match(record)
+    self.records.map! do |record|
+      PARSE_REGEX.match(record)
     end
   end
 
